70ff305c88a48226bb05dacb0f719766227be177
ParkingSoman <80439523+ParkingSoman@users.noreply.github.com>
2021-03-19 09:39:43 -0400

    Update periodictable.py

--- periodictable.py
+++ periodictable.py
@@ -14,7 +14,7 @@ with open(table_path, 'r') as data:
     
 class LookupTable():
     def info(self, element):
-        return [periodic_table[element]['name'], periodic_table[element]['symbol'], periodic_table[element]['number'], periodic_table[element]['atomic_mass'], periodic_table[element]['category]]
+        return [periodic_table[element]['name'], periodic_table[element]['symbol'], periodic_table[element]['number'], periodic_table[element]['atomic_mass'], periodic_table[element]['category']]
     
     # Find all other info by name
     def groupName(self, name):
